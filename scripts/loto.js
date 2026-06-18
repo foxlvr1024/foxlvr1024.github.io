1c93253d9230e7bd1dde5ed05f8ff3e5ca76dcd5
foxlvr1024 <misajovic06@gmail.com>
2026-06-19 00:52:03 +0200

    Buttons now turn red when clicked

--- scripts/loto.js
+++ scripts/loto.js
@@ -3,6 +3,16 @@ const btns = [];
 
 for(let i=0;i<37;i++)
 {
-    btns.push(document.getElementById("btn"+(i+1).toString));
-    //btns[i].addEventListener();
+    let name = "btn"+(i+1).toString();
+    btns.push(document.getElementById(name));
+    if(btns[i] instanceof HTMLElement)
+        btns[i].addEventListener("click",function(){Switch(i)});
+}
+
+
+function Switch(index)
+{
+    
+    if(btns[index] instanceof HTMLElement)
+        btns[index].style.backgroundColor = "red";
 }
