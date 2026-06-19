4836f397d46f920bba942097442dcf3173fc3a53
foxlvr1024 <misajovic06@gmail.com>
2026-06-19 18:22:55 +0200

    Buttons are toggleable

--- scripts/loto.js
+++ scripts/loto.js
@@ -1,18 +1,65 @@
 const btns = [];
-
+const toggled = [];
 
 for(let i=0;i<37;i++)
 {
     let name = "btn"+(i+1).toString();
     btns.push(document.getElementById(name));
+    toggled.push(Boolean(false));
     if(btns[i] instanceof HTMLElement)
+    {
         btns[i].addEventListener("click",function(){Switch(i)});
+        btns[i].addEventListener("mouseenter",function(){Hover(i)});
+        btns[i].addEventListener("mouseleave",function(){Unhover(i)});
+    }
 }
 
 
 function Switch(index)
 {
-    
     if(btns[index] instanceof HTMLElement)
-        btns[index].style.backgroundColor = "red";
+    {
+        //console.log(1);
+        if(toggled[index])
+        {
+            btns[index].style.backgroundColor = "gray";
+        }
+        else
+        {
+            btns[index].style.backgroundColor = "maroon";
+        }
+        toggled[index] = !toggled[index];
+    }
+}
+
+function Hover(index)
+{
+    if(btns[index] instanceof HTMLElement)
+    {
+        //console.log(2);
+        if(toggled[index])
+        {
+            btns[index].style.backgroundColor = "maroon";
+        }
+        else
+        {
+            btns[index].style.backgroundColor = "gray";
+        }
+    }
+}
+
+function Unhover(index)
+{
+    if(btns[index] instanceof HTMLElement)
+    {
+        //console.log(3);
+        if(toggled[index])
+        {
+            btns[index].style.backgroundColor = "red";
+        }
+        else
+        {
+            btns[index].style.backgroundColor = "white";
+        }
+    }
 }
