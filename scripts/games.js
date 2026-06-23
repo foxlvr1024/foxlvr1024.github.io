a5eeac6e56acd79a843e114223cde05b38a3871c
foxlvr1024 <misajovic06@gmail.com>
2026-06-23 23:24:48 +0200

    Added lottery

--- scripts/games.js
+++ scripts/games.js
@@ -5,6 +5,11 @@
 document.getElementById("flipcoin").addEventListener("click",function(){changeURL("flip_coin.html")});
 document.getElementById("flipcoin").style.cursor = "pointer";
 
+document.getElementById("loto").addEventListener("click",function(){changeURL("loto.html")});
+document.getElementById("loto").style.cursor = "pointer";
+
+
+
 function changeURL(newUrl)
 {
     window.location.href = newUrl;
