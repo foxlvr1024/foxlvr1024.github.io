3576e74f8df11a8cf723683a774032b84ebdcc7f
foxlvr1024 <misajovic06@gmail.com>
2026-06-24 19:39:39 +0200

    Added Conways game of life

--- scripts/games.js
+++ scripts/games.js
@@ -8,6 +8,8 @@ document.getElementById("flipcoin").style.cursor = "pointer";
 document.getElementById("loto").addEventListener("click",function(){changeURL("loto.html")});
 document.getElementById("loto").style.cursor = "pointer";
 
+document.getElementById("game_of_life").addEventListener("click",function(){changeURL("game_of_life.html")});
+document.getElementById("game_of_life").style.cursor = "pointer";
 
 
 function changeURL(newUrl)
