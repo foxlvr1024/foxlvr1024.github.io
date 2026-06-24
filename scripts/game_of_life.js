5abd2ad4d7db318261067a625fe1e2b086e669b7
foxlvr1024 <misajovic06@gmail.com>
2026-06-24 19:16:59 +0200

    Game of life works

--- scripts/game_of_life.js
+++ scripts/game_of_life.js
@@ -3,16 +3,21 @@ const btns = [];
 const toggled = [];
 Start();
 document.getElementById("reset").addEventListener("click",Reset);
+document.getElementById("step").addEventListener("click",Step);
 function Start()
 {
     let str = "";
     let display = "";
     for(let i=0;i<63;i++)
     {
-        display += "24px ";
-        for(let j=0;j<34;j++)
+        display+="24px ";
+    }
+    for(let i=0;i<34;i++)
+    {
+        
+        for(let j=0;j<63;j++)
         {
-            let br = i*34+j;
+            let br = i*63+j;
             str+= "<div class=\"cell\" id = \""+br.toString()+"\"></div>\n";
             //console.log(str);
         }
@@ -21,13 +26,14 @@ function Start()
     grid.style.gridTemplateColumns = display;
     grid.innerHTML = str;
 
-    for(let i=0;i<63;i++)
+    for(let i=0;i<34;i++)
     {
         
-        for(let j=0;j<34;j++)
+        for(let j=0;j<63;j++)
         {
             toggled.push(false);
-            let br = i*34+j;
+            let br = i*63+j;
+            console.log(br);
             btns.push(document.getElementById(br.toString()));
             document.getElementById(br.toString()).addEventListener("click",function(){Click(br)});
             document.getElementById(br.toString()).addEventListener("mouseenter",function(){Hover(br)});
@@ -43,7 +49,7 @@ function Click(index)
 {
     if(btns[index] instanceof HTMLElement)
     {
-        //console.log(1);
+        console.log(index);
         if(toggled[index])
         {
             btns[index].style.backgroundColor = "gray";
@@ -91,15 +97,66 @@ function Unhover(index)
 
 function Reset()
 {
-    for(let i=0;i<63;i++)
+    for(let i=0;i<34;i++)
     {
         
-        for(let j=0;j<34;j++)
+        for(let j=0;j<63;j++)
         {
             
-            let br = i*34+j;
+            let br = i*63+j;
             toggled[br]=false;
             btns[br].style.backgroundColor = "white";
         }
     }
+}
+
+
+function Step()
+{
+    const pom = [];
+    for(let i=0;i<34;i++)
+    {
+        
+        for(let j=0;j<63;j++)
+        {
+            
+            let br = i*63+j;
+            let up = ((i+33)%34)*63+j;
+            let down = ((i+1)%34)*63+j;
+            let right = i*63 + ((j+1)%63);
+            let left = i*63 + ((j+62)%63);
+            let lu = ((i+33)%34)*63 + ((j+62)%63);
+            let ld = ((i+1)%34)*63 + ((j+62)%63);
+            let ru = ((i+33)%34)*63 + ((j+1)%63);
+            let rd = ((i+1)%34)*63 + ((j+1)%63);
+            let num = 0;
+            if(toggled[up]) num++;
+            if(toggled[down]) num++;
+            if(toggled[left]) num++;
+            if(toggled[right]) num++;
+            if(toggled[lu]) num++;
+            if(toggled[ld]) num++;
+            if(toggled[ru]) num++;
+            if(toggled[rd]) num++;
+            
+            console.log(br+"-"+num);
+            if(!(toggled[br]) && num == 3) pom.push(true);
+            else if(toggled[br] && num>1 && num<4) pom.push(true);
+            else pom.push(false);
+        }
+    }
+
+    for(let i=0;i<34;i++)
+    {
+        
+        for(let j=0;j<63;j++)
+        {
+            
+            let br = i*63+j;
+            toggled[br]=pom[br];
+            if(toggled[br]) btns[br].style.backgroundColor = "black"; 
+            else btns[br].style.backgroundColor = "white";
+        }
+    }
+
 }
