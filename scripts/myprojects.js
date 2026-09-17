cdb9a93c7cc5e98fd8aa098c643c4e60bcaf376c
foxlvr1024 <misajovic06@gmail.com>
2026-09-17 18:06:26 +0200

    Add Update function that updates based on index and screen size

---
+++ scripts/myprojects.js
@@ -0,0 +1,41 @@
+const games = [];
+let cards = document.getElementById("cards");
+let index = 0;
+
+for(let i=0;i<cards.childElementCount;i++)
+{
+    games.push(cards.children[i]);
+    if(games[i] instanceof HTMLElement)
+    {
+        games[i].style.display = "none";
+    }
+}
+
+
+
+
+
+function Update()
+{
+    if(window.innerWidth<=768)
+    {
+        for(let i=0;i<games.length;i++)
+        {
+            games[i].style.display = "flex";
+        }
+    }
+    else
+    {
+        for(let i=0;i<games.length;i++)
+        {
+            games[i].style.display = "none";
+        }
+        games[index].style.display = "flex";
+        games[index+1].style.display = "flex";
+        games[index+2].style.display = "flex";
+    }
+}
+
+
+
+Update();
